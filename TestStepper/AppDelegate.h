3f3de19923b0f159931a251c7dd3d93e9063440e
mailaldrin29 <mailaldrin29@gmail.com>
2012-07-04 15:49:39 +0530

    second commit

---
+++ TestStepper/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TestStepper
+//
+//  Created by Bharath G M on 7/4/12.
+//  Copyright (c) 2012 Photon Infotech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ TestStepper/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestStepper
+//
+//  Created by Bharath G M on 7/4/12.
+//  Copyright (c) 2012 Photon Infotech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
